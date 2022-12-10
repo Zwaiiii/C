391eb202fa8b1c247fff4152b7a083e7805d0cfe
Zwaiiii <113612979+Zwaiiii@users.noreply.github.com>
2022-12-10 18:18:19 +0900

    Add files via upload

---
+++ CastOperatorApp/Program.cs
@@ -0,0 +1,10 @@
+﻿using System;
+class CastOperatorApp {
+    public static void Main() {
+        int i = 0Xffff;
+        short s;
+        s = (short)i;
+        Console.WriteLine("i = " + i);
+        Console.WriteLine("s = " + s);
+    }
+}
