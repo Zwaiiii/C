bd2dbf9d3bb1fe1d4a4be30057e1643bf133fd1e
Zwaiiii <113612979+Zwaiiii@users.noreply.github.com>
2022-12-10 18:12:36 +0900

    Add files via upload

---
+++ BoxingUnboxingAPp/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+class BoxingUnboxingApp {
+    public static void Main() {
+        int foo = 526;
+        object bar = foo; // 박싱: foo is boxed to bar.
+        Console.WriteLine(bar);
+        try {
+            double d = (short)bar;
+            Console.WriteLine(d);
+        } catch (InvalidCastException e) {
+            Console.WriteLine(e + "Error");
+        }
+    }
+}
